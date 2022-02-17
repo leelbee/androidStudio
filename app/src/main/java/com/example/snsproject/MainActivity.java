0d60bc46c5a76f9c5c73681b28e500ffa64a8dc7
leelbee <dp508856@gmail.com>
2022-02-18 01:05:14 +0900

    22-02-17

--- app/src/main/java/com/example/snsproject/MainActivity.java
+++ app/src/main/java/com/example/snsproject/MainActivity.java
@@ -2,18 +2,28 @@ package com.example.snsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.snsproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+        mainBinding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PostActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
